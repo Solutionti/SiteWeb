1b420d100d422cdf9433f66d58bf7b8b2157d8c2
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-08-31 23:49:34 -0500

    subiendo los cambios

--- app/Views/paginas/inicio.php
+++ app/Views/paginas/inicio.php
@@ -13,9 +13,196 @@
     <link rel="stylesheet" href="../public/css/theme.min.css">
 </head>
 <body>
+  <style>
+    .loader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #071B4D;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
 
+.hidden {
+    display: none;
+}
+  </style>
+  <div id="loader" class="loader">
+    <div class="spinner">
+        <img
+        src="../public/img/logo.png"
+        width="150px;"
+        >
+        <h6 class="text-white  mt-3">ARKANGELES EVENTOS</h6>
+    </div>
+  </div>
+ <header id="header" class="navbar navbar-expand-lg navbar-end navbar-absolute-top navbar-dark">
+   <div class="container mt-3">
+      <nav class="js-mega-menu navbar-nav-wrap">
+        <a class="navbar-brand" href="../index.html" aria-label="Front">
+          <img class="navbar-brand-logo" src="https://htmlstream.com/preview/front-v4.2/html/assets/svg/logos/logo-white.svg" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-default">
+            <i class="fas fa-list"></i>
+          </span>
+          <span class="navbar-toggler-toggled">
+            <i class="fas fa-times"></i>
+          </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <div class="navbar-absolute-top-scroller">
+            <ul class="navbar-nav">
+              
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                INICIO
+              </a>
+              </li>
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                CAMPEONATOS
+              </a>
+              </li>
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                CC STORE
+              </a>
+              </li>
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                CAPACITACIONES
+              </a>
+              </li>
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                BLOG
+              </a>
+              </li>
+              <li class="hs-has-sub-menu nav-item">
+                <a
+                class="nav-link text-white"
+                href="#"
+                role="button"
+              >
+                CONTACTO
+              </a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="btn btn-light btn-transition" href="https://themes.getbootstrap.com/product/front-multipurpose-responsive-template/" target="_blank">Buy now</a>
+              </li>  -->
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main id="content" role="main" class="overflow-hidden">
+    <div class="js-swiper-blog-metro-hero swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide d-lg-flex min-vh-lg-300 gradient-x-overlay-lg-dark bg-img-start content-space-5" style="background-image: url(https://htmlstream.com/preview/front-v4.2/html/assets/img/1920x1080/img8.jpg);">
+          <div class="container">
+            <div class="col-md-6">
+              <a class="text-cap text-white-70" href="#">Adventure</a>
+              <div class="4 mb-md-4">
+                <h2 class="text-white">CAPITAL CHEERS CAMPEONATO NACIONAL</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem odit earum soluta officia, architecto sed, magni est fugit facilis.</p>
+              </div>
+              <a class="btn btn-outline-danger btn-transition" href="#">MAS INFORMACION<i class="bi-chevron-right small ms-1"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide d-lg-flex min-vh-lg-100 gradient-x-overlay-lg-dark bg-img-start content-space-5" style="background-image: url(https://htmlstream.com/preview/front-v4.2/html/assets/img/1920x1080/img22.jpg);">
+          <div class="container d-lg-flex align-items-lg-center">
+            <div class="w-lg-65 text-center mx-lg-auto">
+              <a class="text-cap text-white-70" href="#">Science</a>
+              <div class="mb-4 mb-md-7">
+                <h2 class="h1 text-white">Bonded﻿ by science: researchers describe their nerdy proposals</h2>
+              </div>
+              <a class="btn btn-light btn-transition" href="../blog-article.html">Read more <i class="bi-chevron-right small ms-1"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide d-lg-flex min-vh-lg-100 gradient-x-overlay-lg-dark bg-img-start content-space-5" style="background-image: url(https://htmlstream.com/preview/front-v4.2/html/assets/img/1920x1080/img2.jpg);">
+          <div class="container d-lg-flex align-items-lg-center">
+            <div class="w-lg-65 text-center mx-lg-auto">
+              <a class="text-cap text-white-70" href="#">Environment</a>
+              <div class="mb-4 mb-md-7">
+                <h2 class="h1 text-white">Tree rings reveal increased fire risk for southwestern US</h2>
+              </div>
+              <a class="btn btn-light btn-transition" href="../blog-article.html">Read more <i class="bi-chevron-right small ms-1"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide d-lg-flex min-vh-lg-100 gradient-x-overlay-lg-dark bg-img-start content-space-5" style="background-image: url(https://htmlstream.com/preview/front-v4.2/html/assets/img/1920x1080/img7.jpg);">
+          <div class="container d-lg-flex align-items-lg-center">
+            <div class="w-lg-65 text-center mx-lg-auto">
+              <a class="text-cap text-white-70" href="#">Lifestyle</a>
+              <div class="mb-4 mb-md-7">
+                <h2 class="h1 text-white">2020's Most Relaxing and Affordable Vacations</h2>
+              </div>
+              <a class="btn btn-light btn-transition" href="../blog-article.html">Read more <i class="bi-chevron-right small ms-1"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
+  <!--  -->
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"crossorigin="anonymous"></script>
+  <div class="container content-space-1">
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
+          <a class="text-cap text-danger">Adventure</a>
+            <div class="4 mb-md-4">
+              <h3 class="text-dark">CAPITAL CHEERS CAMPEONATO NACIONAL</h3>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem odit earum soluta officia, architecto sed, magni est fugit facilis.</p>
+            </div>
+              <a class="btn btn-outline-danger btn-transition btn-sm" href="#">VER MAS<i class="bi-chevron-right small ms-1"></i></a>
+        </div>
+    </div>
+  </div>
+
+  <script>
+   window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+
+    setTimeout(() => {
+        clearInterval(loader);
+        loader.style.display = 'none'; // Ocultar el loader
+        content.classList.remove('hidden'); // Mostrar el contenido
+    }, 3000);
+   });
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"crossorigin="anonymous"></script>
+  
 </body>
 </html>
