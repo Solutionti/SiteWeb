a4f27f26c976473ff554ac48989ad124350b9c7f
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2024-08-31 19:30:25 -0500

    subiendo la configuracion de la pagina

---
+++ app/Views/paginas/inicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arkangeles | Inicio</title>
+
+    <link rel="stylesheet" href="../public/fontawesome/css/fontawesome.css" />
+    <link rel="stylesheet" href="../public/fontawesome/css/brands.css" />
+    <link rel="stylesheet" href="../public/fontawesome/css/solid.css" />
+
+    <link rel="stylesheet" href="../public/css/vendor.min.css">
+    <link rel="stylesheet" href="../public/css/theme.min.css">
+</head>
+<body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"crossorigin="anonymous"></script>
+</body>
+</html>
